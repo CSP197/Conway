2ce99e264fa16d93957a3b47b558021bd4618b4f
CSP <16367956+CSP197@users.noreply.github.com>
2023-10-11 00:14:08 -0700

    Adding Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,48 @@
 import React, { useCallback, useState, useRef } from 'react';
 import { produce } from 'immer';
-// import logo from './logo.svg';
 import './App.css';
 
+// Number of rows 
+// and columns for
+// the grid
 const numRows = 50;
 const numCols = 50;
 
+// List of possible
+// directions to
+// check for 
+// neighbors on grid
 const operations = [
-  [ 0,  1],
   [ 0, -1],
+  [ 0,  1],
   [ 1, -1],
-  [-1,  1],
+  [ 1,  0],
   [ 1,  1],
   [-1, -1],
-  [ 1,  0],
   [-1,  0],
+  [-1,  1],
 ];
 
+// 
 const generateEmptyGrid = () => {
   const rows = [];
   for(let i = 0; i <numRows; i++){
-    rows.push(Array.from(Array(numCols), () => 0));
+    rows.push(
+      Array.from(
+        Array(numCols), 
+        () => 0
+      )
+    );
   }
   return rows;
 }
 
+const randomInt = (min: number, max: number) => {
+  // generating a random number
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// 
 function runEpoch( 
   setRunning: React.Dispatch<React.SetStateAction<boolean>>, 
   running: boolean, 
@@ -32,13 +50,13 @@ function runEpoch(
   runSimulation: () => void, 
   alwaysRun?: boolean)
   {
-    if (alwaysRun){
+    if( alwaysRun){
       setRunning(true);
       runningRef.current = true;
       runSimulation();
     } else {
       setRunning(!running);
-      if (!running) {
+      if(!running) {
         runningRef.current = true;
         runSimulation();
       }
@@ -84,15 +102,16 @@ function App() {
         } 
       });
     });
-
     setTimeout(runSimulation, 100);
   }, []);
   return (
     <>
+    <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">Conway's <mark className="px-2 text-white bg-blue-600 rounded dark:bg-blue-500">Game of Life</mark></h1>
+    <div className="inline-flex">
       <button onClick={() => {
         runEpoch(setRunning, running, runningRef, runSimulation);
-        }}>
-        {running ? 'STOP' : 'START'}
+        }} className='text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800'>
+        {running ? 'Stop' : 'Start'}
       </button>
       <button onClick={() => {
         const rows = [];
@@ -101,11 +120,16 @@ function App() {
         }
         setGrid(rows);
         runEpoch(setRunning, running, runningRef, runSimulation, true);
-      }}>RANDOM</button>
+        }} className='text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800'>
+        Random
+      </button>
       <button onClick={() => {
         setGrid(generateEmptyGrid());
         setRunning(false);
-      }}>CLEAR</button>
+        }} className='text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800'>
+        Clear
+      </button>
+    </div>
       <div 
         style={{
           display: 'grid',
@@ -114,7 +138,7 @@ function App() {
       >
         {grid.map(
           (rows, i) => rows.map(
-            (col, j) => (
+            (_, j) => (
               <div 
                 key={`${i}-${j}`}
                 onClick={() => {
@@ -128,7 +152,7 @@ function App() {
                 {
                   width: 20, 
                   height: 20, 
-                  backgroundColor: grid[i][j] ? 'pink' : undefined, 
+                  backgroundColor: grid[i][j] ? `rgb(${randomInt(0,255)} ${randomInt(0,255)} ${randomInt(0,255)} / 0.5)` : undefined, 
                   border: "solid 1px black"
                 }
             }/>
