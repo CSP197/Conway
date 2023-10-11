c501f8efb0b5587ee93a67b4bf32713d47d614a3
CSP <16367956+CSP197@users.noreply.github.com>
2023-10-10 18:03:00 -0700

    Finished conway game of life

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,129 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState, useRef } from 'react';
+import { produce } from 'immer';
 // import logo from './logo.svg';
 import './App.css';
 
 const numRows = 50;
 const numCols = 50;
 
+const operations = [
+  [ 0,  1],
+  [ 0, -1],
+  [ 1, -1],
+  [-1,  1],
+  [ 1,  1],
+  [-1, -1],
+  [ 1,  0],
+  [-1,  0],
+];
+
+const generateEmptyGrid = () => {
+  const rows = [];
+  for(let i = 0; i <numRows; i++){
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+  return rows;
+}
+
 function App() {
   const [grid, setGrid] = useState(() => {
-    const rows = [];
-    for(let i = 0; i <numRows; i++){
-      rows.push(Array.from(Array(numCols), () => 0));
-    }
-    return rows;
+    return generateEmptyGrid();
   });
 
-  console.log(grid);
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSimulation = useCallback(() => {
+    // base case check
+    if( !runningRef.current){
+      return;
+    }
+    //simulate
+    setGrid((g) => {
+      return produce(g, gridCopy => {
+        for( let i = 0; i < numRows; i++){
+          for( let j = 0; j < numCols; j++){
+            let neighbors = 0;
+            // if( gridCopy[i][j+1] === 1){
+            //   neighbors += 1;
+              operations.forEach(([x, y]) => {
+                const newI = i + x;
+                const newJ = j + y;
+                if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+                  neighbors += g[newI][newJ]
+                }
+              });
+
+              if( neighbors < 2 || neighbors > 3){
+                gridCopy[i][j] = 0;
+              } else if( g[i][j] === 0 && neighbors === 3){
+                gridCopy[i][j] = 1;
+              }
+            // }
+          }
+        } 
+      });
+    });
+
+    setTimeout(runSimulation, 100);
+  }, []);
+  // console.log(grid);
   return (
-    <div>
-      yo
-    </div>
+    <>
+      <button onClick={() => {
+        setRunning(!running);
+        if( !running) {
+          runningRef.current = true;
+          runSimulation();
+        }
+        }}>
+        {running ? 'STOP' : 'START'}
+      </button>
+      <button onClick={() => {
+        // setGrid(generateEmptyGrid());
+        const rows = [];
+        for(let i = 0; i <numRows; i++){
+          rows.push(Array.from(Array(numCols), () => Math.random() > 0.8 ? 1 : 0));
+        }
+        setGrid(rows);
+      }}>RANDOM</button>
+      <button onClick={() => {
+        setGrid(generateEmptyGrid());
+        setRunning(false);
+      }}>CLEAR</button>
+      <div 
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${numCols}, 20px)`
+        }}
+      >
+        {grid.map(
+          (rows, i) => rows.map(
+            (col, j) => (
+              <div 
+                key={`${i}-${j}`}
+                onClick={() => {
+                  const newGrid = produce(grid, gridCopy => {
+                    gridCopy[i][j] = gridCopy[i][j] ? 0 : 1;
+                  })
+                  setGrid(newGrid)
+                  // grid[i][k] = 1
+                }}
+                style={
+                {
+                  width: 20, 
+                  height: 20, 
+                  backgroundColor: grid[i][j] ? 'pink' : undefined, 
+                  border: "solid 1px black"
+                }
+            }/>
+          ))
+        )}
+      </div>
+    </>
   );
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.tsx</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
